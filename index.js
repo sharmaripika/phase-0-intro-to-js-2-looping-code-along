695bebece51771fd939c4783bb4c830f4a8cc3fc
Ripika Sharma <ripikasharma@gmail.com>
2023-07-25 02:19:39 -0400

    loop

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names =["Charlie", "Samip", "Ali"];
+const events = "birthday";
+function writeCards(names, events) {
+    const greetings = [];   for (let i = 0; i < names.length; i++) 
+    {const greeting = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+greetings.push(greeting); }
+return greetings;}
+const thankYouGreetings = writeCards(names,events);
+console.log(thankYouGreetings);
+
+
+
+function countDown(num) {
+    while(num >=0) {
+      console.log(num);
+      num--;
+    }
+  }
+  countDown(10);
+  
